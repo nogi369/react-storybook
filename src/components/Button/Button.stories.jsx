9fa237544774dd30506f4fcf7d9aeeab93e1e6f7
nogi <Createc369.Re@gmail.com>
2023-09-17 21:37:15 +0900

    add: ２つのButton作成まで

---
+++ src/components/Button/Button.stories.jsx
@@ -0,0 +1,9 @@
+import Button from "./Button";
+
+export default {
+  title: "Common/Button",
+  component: Button,
+};
+
+export const HelloButton = () => <Button>Hello World!</Button>;
+export const ClickButton = () => <Button>Click!</Button>;
